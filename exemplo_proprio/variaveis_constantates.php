a8bbfb76fe6d419b0b7a61d47f180ac1bfd47fa5
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2023-09-18 21:13:45 -0300

    exemplo proprio

---
+++ exemplo_proprio/variaveis_constantates.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $nome = "Luiz";
+    $sobrenome = "Silva";
+    const PAÍS = "BRASIL";
+
+    echo "Muito Prazer, $nome $sobrenome!Você mora no  " . PAÍS;
+    ?>
+
+</body>
+
+</html>
