4b03ff1f7753ff823ca189d641f2d51f18e839d2
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2023-09-23 17:23:59 -0300

    Manipulacao de string

---
+++ exemplo_proprio/manipulacao_string.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    //echo "Estamos no ano de " .date('Y');
+    $nome = "Rodrigo";
+    $sobrenome = "Nogueira";
+    echo "O nome do lutador de UFC é $nome \"Minotauro \" $sobrenome ";
+
+    /// editor php https://www.w3schools.com/php/phptryit.asp?filename=tryphp_compiler
+    ?>
+
+</body>
+
+</html>
