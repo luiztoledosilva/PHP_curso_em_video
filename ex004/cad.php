afe8f0ecf7027b0a5c870a99ccb02b9db8c00f04
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2023-09-25 20:03:21 -0300

    Expressoes e Operacoes aritiméticas no PHP

--- ex004/cad.php
+++ ex004/cad.php
@@ -5,22 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <header>
         <h1>Resultado do Processamento</h1>
     </header>
-<main>
- <?php 
-//    var_dump($_REQUEST); // $_GET $_POST $_COOKIE
- $n = $_GET["nome"];
- $s = $_GET["sobrenome"];
- echo "É um prazer te conhecer $n $s !!!"
- ?>
+    <main>
+        <?php
+        //    var_dump($_REQUEST); // $_GET $_POST $_COOKIE
+        $nome = $_GET["nome"] ?? "sem nome";
+        $sobrenome = $_GET["sobrenome"] ?? "desconhecido";
+        echo "É um prazer te conhecer <strong>$nome $sobrenome!</strong>Bem vindo ao meu site!! !!!"
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a pagina anterior<a></p>
 
-
-</main>
+    </main>
 
 
 </body>
