2dea5f52172671dab6c5d2f1a72b36b21ec7a8db
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2023-09-24 22:49:41 -0300

    Formularios PHP 2

--- ex004/index.php
+++ ex004/index.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Interação com formularios</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <h1>Apresente-se para nós</h1>
     </header>
-<section>
-  <form action="" method="get"></form>
-  <label for="nome">Nome</label>
-  <input type="text" name="nome" id="nome">
-</section>
+    <section>
+        <form action="cad.php" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="idnome">
+            <label for="sobrenome">Sobrenome</label>
+            <input type="text" name="sobrenome" id="sobrenome">
+            <input type="submit" value="enviar">
+        </form>
+    </section>
 </body>
+
 </html>
